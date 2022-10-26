fb23781c0a689bc164b813d3c6d4431c0f59e544
Big Andy <8012398+big-andy-coates@users.noreply.github.com>
2022-10-26 19:24:51 +0100

    Install & creek-git-diff function

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # creek-script
 Scripts and docs for working with Creek Service repositories
+
+## Installation
+
+To install the scripts, run `. .\install.sh`
+
+## Z-shall Functions
+
+### Git
+
+#### creek-git-diff
+
+Show a diff of all local Creek git repos. All parameters are passed to `git diff`.
+
+For example:
+
+```shell
+> creek-git-diff --name-only
+----- creek-json-schema -------------------
+?? diff.txt
+----- creek-json-schema-gradle-plugin -------------------
+
+.github/workflows/codeql.yml
+----- creek-kafka -------------------
+
+.github/workflows/codeql.yml
+.gitignore
+```
+
+The `?? diff.txt` shows an untracked file in the `creek-json-schema` repo.
+Two other repos have differences.
---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/bin/zsh
+
+#
+# Copyright 2022 Creek Contributors (https://github.com/creek-service)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Installs the Z-shell scripts onto the local machine
+
+SCRIPT_DIR=${0:a:h}/zsh-functions
+
+echo >> ~/.zshrc
+echo "# Root directory containing Creek repos:" >> ~/.zshrc
+echo "CREEK_BASE_DIR=$(dirname $PWD)" >> ~/.zshrc
+echo "Set CREEK_BASE_DIR to $(dirname $PWD)"
+
+echo >> ~/.zshrc
+echo "# Install Creek functions:" >> ~/.zshrc
+
+find $SCRIPT_DIR -type d ! -empty -exec sh -c "
+  echo "fpath+={}" >> ~/.zshrc
+  echo added {} to fpath
+  " \;
+
+echo "autoload -Uz $SCRIPT_DIR/**/*(.:t)" >> ~/.zshrc
+echo "typeset -U fpath" >> ~/.zshrc
+source ~/.zshrc
